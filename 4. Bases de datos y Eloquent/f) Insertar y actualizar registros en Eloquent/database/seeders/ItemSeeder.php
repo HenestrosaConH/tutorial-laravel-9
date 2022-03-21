135758e34badedba49d48ee5c37ed6ddd2e09984
josecarlosLH <josecarlos.lopez@internaliagroup.com>
2022-03-21 18:53:03 +0100

    Factories añadidos

--- 4. Bases de datos y Eloquent/f) Insertar y actualizar registros en Eloquent/database/seeders/ItemSeeder.php
+++ 4. Bases de datos y Eloquent/f) Insertar y actualizar registros en Eloquent/database/seeders/ItemSeeder.php
@@ -4,16 +4,17 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use \App\Models\Item;
 
-class DatabaseSeeder extends Seeder
+class ItemSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call(ItemSeeder::class);
+        Item::factory(50)->create();
     }
 }
